ef7edef0acc547f6a1f0ada31ab27e38a37e9517
Sarah Cyr <sarahcyrdesign@gmail.com>
2017-07-08 01:10:04 +0000

    Successfully scraped needed games and attributes. Added attributes to game class

--- lib/nes_collector/game.rb
+++ lib/nes_collector/game.rb
@@ -1,3 +1,14 @@
 class Game
+  attr_accessor :name, :loose_price, :cib_price, :new_price
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+  @@all << self
+  end
 
 end
--- lib/nes_collector/scraper.rb
+++ lib/nes_collector/scraper.rb
@@ -1,3 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class Scraper
 
+  def self.scrape_page
+    doc = Nokogiri::HTML(open("https://www.pricecharting.com/console/nes"))
+    name = doc.css("td.title").first.text #Super Mario 3
+    doc.css("td.price.numeric.used_price").first.text #$10.50
+    doc.css("td.price.numeric.cib_price").first.text #$27.06
+    doc.css("td.price.numeric.new_price").first.text #$200.00
+  # binding.pry
+  end
+
 end
+
+Scraper.scrape_page
